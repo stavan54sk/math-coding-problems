a15b0357dc7d5abe51af9f9cc593f2cde8d33ca2
stavan54sk <stavan54sk@gmail.com>
2025-01-24 07:08:14 +0530

    init

---
+++ src/main/java/com/mymath/App.java
@@ -0,0 +1,13 @@
+package com.mymath;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
